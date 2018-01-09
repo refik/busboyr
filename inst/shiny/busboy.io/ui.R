dac386840609a9268e32fc197a4614575d0482d5
Refik Turkeli <refik.rfk@gmail.com>
2018-01-09 10:01:26 +0300

    debugging request headers

--- inst/shiny/busboy.io/ui.R
+++ inst/shiny/busboy.io/ui.R
@@ -1,27 +1,34 @@
-shiny::tagList(
-    tags$head(
-        tags$script(src = "js.cookie.min.js"),
-        tags$script(src = "busboy.js"),
-        tags$link(rel = "stylesheet", type = "text/css", href = "busboy.css"),
-        shiny::HTML('
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-            <link rel="manifest" href="/manifest.json">
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b53131">
-            <meta name="theme-color" content="#ffffff">
-        ')
-    ),
-    shinyjs::useShinyjs(),
-    shiny::navbarPage(
-        title = shiny::tags$img(src = "logo.png"), 
-        theme = "paper_theme.css", selected = "account", 
-        id = "busboy_navbar", windowTitle = "Busboy", fluid = FALSE,
-        shiny::tabPanel("Account", value = "account",
-                        account_UI("account")),
-        shiny::tabPanel("Search", value = "search",
-                        search_UI("search")),
-        shiny::tabPanel("Title Information", value = "title",
-                        title_UI("title"))
+function(request) {
+    shiny::tagList(
+        tags$head(
+            tags$script(src = "js.cookie.min.js"),
+            tags$script(src = "busboy.js"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "busboy.css"),
+            shiny::HTML('
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                <link rel="manifest" href="/manifest.json">
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b53131">
+                <meta name="theme-color" content="#ffffff">
+            ')
+        ),
+        shinyjs::useShinyjs(),
+        shiny::navbarPage(
+            title = shiny::tags$img(src = "logo.png"), 
+            theme = "paper_theme.css", selected = "account", 
+            id = "busboy_navbar", windowTitle = "Busboy", fluid = FALSE,
+            shiny::tabPanel("Account", value = "account",
+                            account_UI("account"),
+                            page_header_title("UI Request Object"),
+                            shiny::tags$pre(
+                                paste(utils::capture.output(print(as.list(request))), 
+                                      collapse = "\n")
+                            )),
+            shiny::tabPanel("Search", value = "search",
+                            search_UI("search")),
+            shiny::tabPanel("Title Information", value = "title",
+                            title_UI("title"))
+        )
     )
-)
+}
